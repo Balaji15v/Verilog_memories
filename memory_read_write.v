fbb2da0512c64bfe6e8e7533aa0e61f9be509a54
Balaji <amgothbalaji2000@gmail.com>
2026-06-26 14:50:08 +0530

    memory_read_write.v

---
+++ memory_read_write.v
@@ -0,0 +1,14 @@
+module example(input rst,clk,wr,sel,input [15:0]wdata, output [15:0]rdata);
+reg [15:0] register;
+always @(posedge clk) begin
+    if(rst)
+    register<=0;
+    else begin 
+        if(sel && wr)
+         register<=wdata;
+        else
+        register<=register;
+    end
+end
+assign rdata=(sel & ~wr)?register:0;
+endmodule
